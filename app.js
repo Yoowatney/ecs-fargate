d2f8e498fc1c5031f3296478d9ed9d2410538073
yoyoo <ysyoo@pureblack.co.kr>
2023-06-26 16:28:46 +0900

    ✨ ADD : task defintion

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World - 2");
+});
+
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
